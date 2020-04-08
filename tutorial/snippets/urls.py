f3d914c5851378ee4ccd11fabb00fd1d34d28ea8
anant <anantdevdubey@hotmail.com>
2020-04-08 19:45:04 +0530

    created a basic api, which uses ModelSerializers

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+]
+
